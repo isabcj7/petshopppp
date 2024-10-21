39a2f012c3ef4573109507865a21783e1e1fed43
isabcj7 <isabela.jeronymo@gmail.com>
2024-10-21 15:02:07 -0300

    Add files via upload

---
+++ PrjIsabelaCostaJeronymoSomativa1demoApplication.java
@@ -0,0 +1,13 @@
+package com.IsabelaCostaJeronymo.cad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjIsabelaCostaJeronymoSomativa1demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjIsabelaCostaJeronymoSomativa1demoApplication.class, args);
+	}
+
+}
